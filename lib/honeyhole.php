088bb752e4037cb70c54e3c1feffbd0df2a5b951
clayball <cwells73@gmail.com>
2016-05-26 18:27:43 -0400

    adding empty honeyhole class.

---
+++ lib/honeyhole.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: clayball
+ *
+ * This is honeyhole's main class
+ */
+class honeyhole
+{
+
+    function get_data()
+    {
+        // get data from honeypot logs
+
+    }
+
+    
+}
